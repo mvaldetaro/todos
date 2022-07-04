68ea34fe7535254f9cdad2fdadb87852d5d7cd64
Magno Valdetaro <mvaldetaro@gmail.com>
2022-07-04 14:39:51 -0300

    Implementa todo com Svelte

---
+++ svelte-app/src/lib/TodoList.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from "svelte";
+
+  let todos = [];
+  let todoText = "";
+
+  onMount(() => {
+    todos = JSON.parse(localStorage.getItem("todos")) || [];
+  });
+
+  function addTodo() {
+    todos = [...todos, todoText];
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+</script>
+
+<main>
+  <ul>
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+  <form on:submit|preventDefault={addTodo}>
+    <input type="text" bind:value={todoText} placeholder="Digite sua tarefa" />
+    <button type="submit">Adicionar Tarefa</button>
+  </form>
+</main>
+
+<style>
+</style>
