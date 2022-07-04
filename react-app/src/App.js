99d7ce99abb7a2d04175204d340f2ffffd93fb82
Magno Valdetaro <mvaldetaro@gmail.com>
2022-07-04 11:31:21 -0300

    Implementa todo com reactjs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  // State
+
+  const [todos, setTodos] = useState([]);
+
+  // Binding
+  const todoText = useRef();
+
+  // Events
+
+  function addTodo(e) {
+    e.preventDefault();
+    const updatedList = [...todos, todoText.current.value];
+    setTodos(updatedList);
+    localStorage.setItem("todos", JSON.stringify(updatedList));
+  }
+
+  // Side Effects (lifecycle hooks)
+
+  useEffect(() => {
+    const storageTodos = localStorage.getItem("todos");
+    setTodos(storageTodos ? JSON.parse(storageTodos) : []);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" placeholder="Digite sua tarefa" ref={todoText} />
+        <button type="submit">Adicionar Tarefa</button>
+      </form>
+    </>
   );
 }
 
