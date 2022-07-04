beeb6e8d9b2f58e35909cbb94f32c8243db9e54a
Magno Valdetaro <mvaldetaro@gmail.com>
2022-07-04 14:13:42 -0300

    Implementa todo com vue

---
+++ vue-app/src/components/Todo.vue
@@ -0,0 +1,35 @@
+<script>
+  export default {
+    name: "Todo",
+    data: function () {
+      return {
+        todos: [],
+        todoText: ''
+      };
+    },
+    methods: {
+      addTodo: function () {
+        this.todos = [...this.todos, this.todoText];
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      },
+    },
+    mounted: function () {
+      this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+      console.log("Teste");
+    }
+  }
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todos" v-bind:key="todo">{{todo}}</li>
+  </ul>
+  <form v-on:submit.prevent="addTodo">
+    <input type="text" v-model="todoText" placeholder="Digite sua tarefa" />
+    <button type="submit">Adicionar</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
